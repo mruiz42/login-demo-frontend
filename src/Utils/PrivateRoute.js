838770ab47a59e00eb14e76ae50e61611066da84
Michael Ruiz <michael@mruiz.dev>
2021-01-05 19:21:26 -0800

    restored back to non context api

--- src/Utils/PrivateRoute.js
+++ src/Utils/PrivateRoute.js
@@ -4,7 +4,6 @@ import { isLogin, verifySession } from './Common';
 
 const PrivateRoute = ({component: Component, ...rest}) => {
   return (
-
       // Show the component only when the user is logged in
       // Otherwise, redirect the user to /signin page
       <Route {...rest} render={props => (
